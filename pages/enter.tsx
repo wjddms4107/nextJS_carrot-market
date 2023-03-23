34551fa47a6e9b7749953d81967ad7cab66176cb
jeongeun <wjddms4107@gmail.com>
2023-03-23 21:28:41 +0900

    add : 컴포넌트화

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,11 +1,13 @@
+import type { NextPage } from "next";
 import { useState } from "react";
+import Button from "../components/button";
+import Input from "../components/input";
 import { cls } from "../libs/utils";
 
-export default function Enter() {
+const Enter: NextPage = () => {
   const [method, setMethod] = useState<"email" | "phone">("email");
   const onEmailClick = () => setMethod("email");
   const onPhoneClick = () => setMethod("phone");
-
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
@@ -37,32 +39,25 @@ export default function Enter() {
             </button>
           </div>
         </div>
-        <form className="flex flex-col mt-8">
-          <label htmlFor="input" className="text-sm font-medium text-gray-700">
-            {method === "email" ? "Email address" : null}
-            {method === "phone" ? "Phone number" : null}
-          </label>
-          <div className="mt-1">
-            {method === "email" ? <input id="input" type="email" className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500" required /> : null}
-            {method === "phone" ? (
-                 <div className="flex rounded-md shadow-sm">
-                 <span className="flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm">
-                   +82
-                 </span>
-                 <input
-                   id="input"
-                   type="number"
-                   className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-                   required
-                 />
-              </div>
-            ) : null}
-          </div>
-          <button className="mt-5 bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none ">
-            {method === "email" ? "Get login link" : null}
-            {method === "phone" ? "Get one-time password" : null}
-          </button>
+        <form className="flex flex-col mt-8 space-y-4">
+          {method === "email" ? (
+            <Input name="email" label="Email address" type="email" required />
+          ) : null}
+          {method === "phone" ? (
+            <Input
+              name="phone"
+              label="Phone number"
+              type="number"
+              kind="phone"
+              required
+            />
+          ) : null}
+          {method === "email" ? <Button text={"Get login link"} /> : null}
+          {method === "phone" ? (
+            <Button text={"Get one-time password"} />
+          ) : null}
         </form>
+
         <div className="mt-8">
           <div className="relative">
             <div className="absolute w-full border-t border-gray-300" />
@@ -102,4 +97,5 @@ export default function Enter() {
       </div>
     </div>
   );
-}
+};
+export default Enter;
--- pages/profile/bought.tsx
+++ pages/profile/bought.tsx
@@ -1,63 +1,39 @@
 import type { NextPage } from "next";
+import Button from "../../components/button";
+import Input from "../../components/input";
+import Layout from "../../components/layout";
 
-const Bought: NextPage = () => {
+const EditProfile: NextPage = () => {
   return (
-    <div className="flex flex-col space-y-5 py-10">
-      {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-        <div
-          key={i}
-          className="flex px-4  border-b pb-5 cursor-pointer justify-between"
-        >
-          <div className="flex space-x-4">
-            <div className="w-20 h-20 bg-gray-400 rounded-md" />
-            <div className="pt-2 flex flex-col">
-              <h3 className="text-sm font-medium text-gray-900">
-                New iPhone 14
-              </h3>
-              <span className="text-xs text-gray-500">Black</span>
-              <span className="font-medium mt-1 text-gray-900">$95</span>
-            </div>
-          </div>
-          <div className="flex space-x-2 items-end justify-end">
-            <div className="flex space-x-0.5 items-center text-sm  text-gray-600">
-              <svg
-                className="w-4 h-4"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                ></path>
-              </svg>
-              <span>1</span>
-            </div>
-            <div className="flex space-x-0.5 items-center text-sm  text-gray-600">
-              <svg
-                className="w-4 h-4"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                ></path>
-              </svg>
-              <span>1</span>
-            </div>
-          </div>
+    <Layout canGoBack title="Edit Profile">
+      <form className="py-10 px-4 space-y-4">
+        <div className="flex items-center space-x-3">
+          <div className="w-14 h-14 rounded-full bg-slate-500" />
+          <label
+            htmlFor="picture"
+            className="cursor-pointer py-2 px-3 border hover:bg-gray-50 border-gray-300 rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 text-gray-700"
+          >
+            Change
+            <input
+              id="picture"
+              type="file"
+              className="hidden"
+              accept="image/*"
+            />
+          </label>
         </div>
-      ))}
-    </div>
+        <Input required label="Email address" name="email" type="email" />
+        <Input
+          required
+          label="Phone number"
+          name="phone"
+          type="number"
+          kind="phone"
+        />
+        <Button text="Update profile" />
+      </form>
+    </Layout>
   );
 };
 
-export default Bought;
+export default EditProfile;

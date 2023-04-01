70d7edbbe302595eb10b9b7ec53ae72332ad7c64
jeongeun <wjddms4107@gmail.com>
2023-04-01 17:11:22 +0900

    add : onValid 함수 생성

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -4,30 +4,42 @@ import { useForm } from "react-hook-form";
 import Button from "../components/button";
 import Input from "../components/input";
 import { cls } from "../libs/utils";
-
 interface EnterForm {
-  email?:string;
-  phone?:string;
+  email?: string;
+  phone?: string;
 }
 
 const Enter: NextPage = () => {
-  const {register, watch, handleSubmit, reset} = useForm<EnterForm>();
+  const [submitting, setSubmitting] = useState(false);
+  const { register, handleSubmit, reset } = useForm<EnterForm>();
   const [method, setMethod] = useState<"email" | "phone">("email");
-  const onEmailClick = () => {reset(); setMethod("email");}
-  const onPhoneClick = () =>{reset(); setMethod("phone");} 
-
-  const onValid = (data:EnterForm) => {
-    console.log(data)
-  }
-
-  console.log(watch())
+  const onEmailClick = () => {
+    reset();
+    setMethod("email");
+  };
+  const onPhoneClick = () => {
+    reset();
+    setMethod("phone");
+  };
+  const onValid = (data: EnterForm) => {
+    setSubmitting(true);
+    fetch("/api/users/enter", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => {
+      setSubmitting(false);
+    });
+  };
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
       <div className="mt-12">
         <div className="flex flex-col items-center">
           <h5 className="text-sm text-gray-500 font-medium">Enter using:</h5>
-          <div className="grid  border-b  w-full mt-8 grid-cols-2 ">
+          <div className="grid border-b  w-full mt-8 grid-cols-2 ">
             <button
               className={cls(
                 "pb-4 font-medium text-sm border-b-2",
@@ -52,13 +64,24 @@ const Enter: NextPage = () => {
             </button>
           </div>
         </div>
-        <form onSubmit={handleSubmit(onValid)} className="flex flex-col mt-8 space-y-4">
+        <form
+          onSubmit={handleSubmit(onValid)}
+          className="flex flex-col mt-8 space-y-4"
+        >
           {method === "email" ? (
-            <Input register={register("email", {required:true})} name="email" label="Email address" type="email" />
+            <Input
+              register={register("email", {
+                required: true,
+              })}
+              name="email"
+              label="Email address"
+              type="email"
+              required
+            />
           ) : null}
           {method === "phone" ? (
             <Input
-            register={register("phone")}
+              register={register("phone")}
               name="phone"
               label="Phone number"
               type="number"
@@ -68,7 +91,7 @@ const Enter: NextPage = () => {
           ) : null}
           {method === "email" ? <Button text={"Get login link"} /> : null}
           {method === "phone" ? (
-            <Button text={"Get one-time password"} />
+            <Button text={submitting ? "Loading" : "Get one-time password"} />
           ) : null}
         </form>
 

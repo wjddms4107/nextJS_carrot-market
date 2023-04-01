859b09bfbd46c5ff585d9626beefa9d00bfa4fdd
jeongeun <wjddms4107@gmail.com>
2023-04-01 16:46:57 +0900

    refactor: enter to carrot react-hook-form 적용

--- components/input.tsx
+++ components/input.tsx
@@ -1,15 +1,22 @@
+import type { UseFormRegisterReturn } from "react-hook-form";
+
 interface InputProps {
     label: string;
     name: string;
     kind?: "text" | "phone" | "price";
-    [key: string]: any;
+    type: string;
+    register: UseFormRegisterReturn;
+    required: boolean;
+    
   }
   
   export default function Input({
     label,
     name,
+    type,
     kind = "text",
-    ...rest
+    register,
+    required
   }: InputProps) {
     return (
       <div>
@@ -23,7 +30,9 @@ interface InputProps {
           <div className="rounded-md relative flex  items-center shadow-sm">
             <input
               id={name}
-              {...rest}
+              required={required}
+              {...register}
+              type={type}
               className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
             />
           </div>
@@ -35,7 +44,9 @@ interface InputProps {
             </div>
             <input
               id={name}
-              {...rest}
+              required={required}
+              {...register}
+              type={type}
               className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
             />
             <div className="absolute right-0 pointer-events-none pr-3 flex items-center">
@@ -50,7 +61,9 @@ interface InputProps {
             </span>
             <input
               id={name}
-              {...rest}
+              required={required}
+              {...register}
+              type={type}
               className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
             />
           </div>

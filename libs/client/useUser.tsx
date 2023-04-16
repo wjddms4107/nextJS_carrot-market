13eb992058d88c8d5edf5b31acde21b30131ff2d
노정은 <wjddms4107@naver.com>
2023-04-16 23:01:06 +0900

    add : 프로필 수정 페이지 api 연결 및 구현

--- libs/client/useUser.tsx
+++ libs/client/useUser.tsx
@@ -1,12 +1,15 @@
+import { User } from "@prisma/client";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import useSWR from "swr";
 
-const fetcher = (url: string) => fetch(url).then((response) => response.json());
+interface ProfileResponse {
+  ok: boolean;
+  profile: User;
+}
 
 export default function useUser() {
-  const { data, error } = useSWR("/api/users/me");
-
+  const { data, error } = useSWR<ProfileResponse>("/api/users/me");
   const router = useRouter();
 
   useEffect(() => {
@@ -14,8 +17,6 @@ export default function useUser() {
       router.replace("/enter");
     }
   }, [data, router]);
-
-
   
   return { user: data?.profile, isLoading: !data && !error };
 }

a3a532e5cf35292505aa8477e2dcdf0d471e46b1
노정은 <wjddms4107@naver.com>
2023-04-22 15:18:31 +0900

    add : product upload 이미지 미리보기 기능 추가

--- pages/products/upload.tsx
+++ pages/products/upload.tsx
@@ -5,7 +5,7 @@ import Layout from "@components/layout";
 import TextArea from "@components/textarea";
 import { useForm } from "react-hook-form";
 import useMutation from "@libs/client/useMutation";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Product } from "@prisma/client";
 import { useRouter } from "next/router";
 
@@ -13,6 +13,7 @@ interface UploadProductForm {
   name: string;
   price: number;
   description: string;
+  photo: FileList;
 }
 
 interface UploadProductMutation {
@@ -22,12 +23,12 @@ interface UploadProductMutation {
 
 const Upload: NextPage = () => {
   const router = useRouter();
-  const { register, handleSubmit } = useForm<UploadProductForm>();
+  const { register, handleSubmit,watch } = useForm<UploadProductForm>();
 
   const [uploadProduct, { loading, data }] =
   useMutation<UploadProductMutation>("/api/products");
 
-  const onValid = (data: UploadProductForm) => {
+  const onValid = async ({ name, price, description }: UploadProductForm) => {
     if (loading) return;
     uploadProduct(data);
   };
@@ -38,27 +39,48 @@ const Upload: NextPage = () => {
     }
   }, [data, router]);
 
+  const photo = watch("photo");
+  const [photoPreview, setPhotoPreview] = useState("");
+  useEffect(() => {
+    if (photo && photo.length > 0) {
+      const file = photo[0];
+      setPhotoPreview(URL.createObjectURL(file));
+    }
+  }, [photo]);
+
   return (
     <Layout canGoBack title="Upload Product">
       <form className="p-4 space-y-4" onSubmit={handleSubmit(onValid)}>
         <div>
-          <label className="w-full cursor-pointer text-gray-600 hover:border-orange-500 hover:text-orange-500 flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md">
-            <svg
-              className="h-12 w-12"
-              stroke="currentColor"
-              fill="none"
-              viewBox="0 0 48 48"
-              aria-hidden="true"
-            >
-              <path
-                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
+        {photoPreview ? (
+            <img
+              src={photoPreview}
+              className="w-full text-gray-600  h-46 rounded-md"
+            />
+          ) : (
+            <label className="w-full cursor-pointer text-gray-600 hover:border-orange-500 hover:text-orange-500 flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md">
+              <svg
+                className="h-12 w-12"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 48 48"
+                aria-hidden="true"
+              >
+                <path
+                  d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+              <input
+                {...register("photo")}
+                accept="image/*"
+                className="hidden"
+                type="file"
               />
-            </svg>
-            <input className="hidden" type="file" />
-          </label>
+            </label>
+          )}
         </div>
         <Input
           register={register("name", { required: true })}
